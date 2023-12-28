ac148008cacc25642c58f1f364695feea2af58b6
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2023-12-28 09:27:17 +0530

    Add files via upload

---
+++ Programs/Conditional_Control/DigRev.java
@@ -0,0 +1,18 @@
+public class DigRev
+{
+	public static void main(String args[])
+	{
+		int No = 1906,Rev = 0,Rem = 0;
+		System.out.println("Given Number = "+ No);
+		while(No > 0)
+		{
+			Rem = No % 10;
+			Rev = (Rev*10)+Rem;
+        		No  = No / 10;
+		}
+		
+		System.out.println("Reverse Number = "+Rev);
+
+		
+	}
+}
---
+++ Programs/Conditional_Control/DigSep.java
@@ -0,0 +1,17 @@
+public class DigSep
+{
+	public static void main(String args[])
+	{
+		int No = 1906,Temp = 0;
+
+		while(No > 0)
+		{
+			Temp = No%10;
+			System.out.println("\n\t"+Temp);
+			No  = No/10;	
+		}
+
+	}
+}
+ 
+//Important For Exam
