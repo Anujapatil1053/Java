e8546846e99023f4aeab0e89e1bd9c8772d94127
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2023-12-28 09:31:34 +0530

    Add files via upload

---
+++ Programs/Conditional_Control/ForLoop.java
@@ -0,0 +1,23 @@
+public class ForLoop
+{
+	public static void main(String args[])
+	{
+		//String A = "INDIA";
+		//for(int i = 1; i <= 10; i++)
+		//{
+			//System.out.println(A);
+		//}
+		//int i = 0;
+		//for(i = 1; i <= 5; i++)
+		//{
+			//System.out.println("\n\t INDIA");
+		//}
+		
+		String Str = "INDIA";                   
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println( i+ ". " +Str);
+		}
+
+	}
+}
