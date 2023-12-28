58882958689ed83e5d9721045e4321e3d58e624f
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2023-12-28 09:25:36 +0530

    Add files via upload

---
+++ Programs/Conditional_Control/Armstrong.java
@@ -0,0 +1,30 @@
+public class Armstrong
+{
+	public static void main(String args[])
+	{
+		int No = 1991, Dig = 0, DCnt = 0, Temp = 0,Res = 0;
+		Temp = No;
+		while(Temp > 0)
+		{
+			Temp= Temp /10; 
+			DCnt++;
+			
+		}
+		Temp = No;
+		while(Temp > 0)
+		{
+			Dig = Temp % 10;
+			Res = Res+((int)Math.pow(Dig,DCnt));
+			Temp = Temp / 10; 
+		}
+		if(No == Res)
+		{
+			System.out.println("Given Number Is Armstrong Number ");
+		}
+		else
+		{
+			System.out.println("Given Number Is Not Armstrong Number ");
+		}
+		
+	}
+}
---
+++ Programs/Conditional_Control/Cube.java
@@ -0,0 +1,15 @@
+public class Cube
+{
+	public static void main(String args[])
+	{
+		int No = 5,Cube = 0;
+		
+		
+		Cube = 	No * No * No;
+			
+		
+		
+		System.out.println(Cube);
+
+	}
+}
