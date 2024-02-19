c7820c336ae6dc4fbcf035af507d3a6952390ea2
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2024-02-19 19:58:21 +0530

    Java

--- Programs/Conditional_Control/DigSep.java
+++ Programs/Conditional_Control/DigSep.java
@@ -3,14 +3,12 @@ public class DigSep
 	public static void main(String args[])
 	{
 		int No = 1906,Temp = 0;
-
 		while(No > 0)
 		{
 			Temp = No%10;
 			System.out.println("\n\t"+Temp);
 			No  = No/10;	
 		}
-
 	}
 }
  
