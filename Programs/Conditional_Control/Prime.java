166c9639e9ef7e19a8864c2adaa7aa65d8ce4545
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2024-03-04 22:27:20 +0530

    Java

--- Programs/Conditional_Control/Prime.java
+++ Programs/Conditional_Control/Prime.java
@@ -2,21 +2,18 @@ public class Prime
 {
 	public static void main(String args[])
 	{
-		int No = 29,Cnt = 0;                  
+		int No = 29, Cnt = 0;                  
 		
 		for(int i = 2; Cnt /2; i++)
 		{
 			if(No % Cnt == 0)
-		{
-			System.out.println("It Is Prime Number");
-		}
-		else
-		{	
-			System.out.println("It Is Prime Number");
-		}
-		}
-		
+			{
+				System.out.println("It Is Prime Number");
+			}
+			else
+			{	
+				System.out.println("It Is Prime Number");
+			}
+		}	
 	}
-
-
 }
