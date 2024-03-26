81fba5cbc04016c4933389f3e0b836f779bb872a
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2024-03-26 21:47:39 +0530

    Java

--- Programs/Conditional_Control/DigitCount.java
+++ Programs/Conditional_Control/DigitCount.java
@@ -3,12 +3,14 @@ public class DigitCount
 	public static void main(String args[])
 	{
 		int No = 6767676, Dig = 0, DCnt = 0, Temp = No;
+		
 		while(Temp > 0)
 		{
 			Dig = Temp % 10;
 			DCnt++;
 			Temp= Temp /10; 
 		}
+		
 		System.out.println(DCnt);
 	}
 }
